bdff8613a26ad4b80a42665c9abe041f97d8331f
David <david.v.rumba@gmail.com>
2025-08-06 17:05:56 +0200

    Añadidas funciones en GestorClientes

--- GestorClientes.java
+++ GestorClientes.java
@@ -6,4 +6,54 @@ public class GestorClientes {
     public GestorClientes() {
         clientes = new ArrayList<>();
     }
+
+    public boolean altaCliente(Cliente cliente) {
+        for (Cliente c : clientes) {
+            if (c.getDni().equals(cliente.getDni())) {
+                return false;
+            }
+        }
+        clientes.add(cliente);
+        return true;
+    }
+
+    public boolean bajaCliente(String dni) {
+        for (int i = 0; i < clientes.size(); i++) {
+            Cliente c = clientes.get(i);
+            if (c.getDni().equals(dni)) {
+                clientes.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean modificarCliente(String dni,
+                                    String nuevoNombre,
+                                    String nuevoTelefono,
+                                    String nuevoEmail) {
+        for (Cliente c : clientes){
+            if (c.getDni().equals(dni)) {
+                c.setNombre(nuevoNombre);
+                c.setTelefono(nuevoTelefono);
+                c.setEmail(nuevoEmail);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Cliente buscarCliente(String dni) {
+        for (Cliente c : clientes) {
+            if (c.getDni().equals(dni)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Cliente> listarClientes() {
+        return new ArrayList<>(clientes);
+    }
+
 }
